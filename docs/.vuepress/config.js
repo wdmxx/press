0d07b356a87f7591f0ccec033d33700d75854dc9
wdm_xx_0704 <chaina152478@163.com>
2019-03-04 23:16:47 +0800

    好多

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,29 @@
 module.exports = {
 	title: 'wdm个人小记',
 	description: 'blog',
-
+	base: '/press/',
 	themeConfig:{
 		nav: [
 			{ text: 'HOME', link: '/'},
+			{ text: '机器学习', link: '/machine_learning/'},
 			{ text: 'SQL', link:'/sql/'},
 			{ text: 'Python', link:'/python/'}
 		],
 
-		sidebar: [
-					'/sql/sql.start',
-					'/sql/sql.use'
+		sidebar: {
+			'/sql/':[
+				{
+					title: 'SQL',
+					children:[
+						'/sql/sql.start',
+						'/sql/sql.use'
+					]
+				}
+			]
+		}
+					
 
-			
-				]		
+				
 
 		},		
 	
